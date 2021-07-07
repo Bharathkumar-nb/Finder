1cdf1beda13413c6bed4362f9a390a588f309bab
Bharathkumar-nb <2036032+Bharathkumar-nb@users.noreply.github.com>
2021-07-07 18:08:08 -0400

    FIN-7 Added CameraPreviewActivity

---
+++ app/src/main/java/com/example/finder/CameraPreviewActivity.java
@@ -0,0 +1,98 @@
+package com.example.finder;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+
+public class CameraPreviewActivity extends AppCompatActivity {
+
+    private static final String[] REQUIRED_PERMISSIONS = new String[] {"android.permission.CAMERA"};
+    private static final int REQUEST_CODE_PERMISSIONS = 10;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_camera_preview);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        if (allPermissionGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+    }
+
+    private boolean allPermissionGranted() {
+        for(String permission: REQUIRED_PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                this.finish();
+            }
+        }
+    }
+
+    private void startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        PreviewView cameraPreviewView = findViewById(R.id.cameraPreviewView);
+        preview.setSurfaceProvider(cameraPreviewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+    }
+
+    public void goBack(View view) {
+        this.finish();
+    }
+}
--- app/src/main/java/com/example/finder/MainActivity.java
+++ app/src/main/java/com/example/finder/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.finder;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,10 +13,34 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.example.finder.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
+        Button findButton = findViewById(R.id.findButton);
+        findButton.setOnClickListener(v -> {
+            if (validateSearchText()) {
+                Intent intent = new Intent(v.getContext(), CameraPreviewActivity.class);
+                EditText searchText = findViewById(R.id.searchText);
+                String message = searchText.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            } else {
+                Context context = getApplicationContext();
+                CharSequence errorText = "Invalid Text!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, errorText, duration);
+                toast.show();
+            }
+        });
+    }
+
+    private boolean validateSearchText() {
+        EditText searchText = findViewById(R.id.searchText);
+        return searchText.getText().length() != 0;
     }
 }
