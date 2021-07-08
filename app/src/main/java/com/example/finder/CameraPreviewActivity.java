5fb31aa6ac15ca6bd4ba96caf83760c3731f7cca
Bharathkumar-nb <2036032+Bharathkumar-nb@users.noreply.github.com>
2021-07-07 22:41:50 -0400

    FIN-8 Added image analyzer

--- app/src/main/java/com/example/finder/CameraPreviewActivity.java
+++ app/src/main/java/com/example/finder/CameraPreviewActivity.java
@@ -1,32 +1,50 @@
 package com.example.finder;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Size;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.TextRecognizerOptions;
 
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CameraPreviewActivity extends AppCompatActivity {
 
     private static final String[] REQUIRED_PERMISSIONS = new String[] {"android.permission.CAMERA"};
     private static final int REQUEST_CODE_PERMISSIONS = 10;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private final Executor executor = Executors.newSingleThreadExecutor();
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +53,7 @@ public class CameraPreviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera_preview);
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         if (allPermissionGranted()) {
             startCamera();
         } else {
@@ -88,11 +106,71 @@ public class CameraPreviewActivity extends AppCompatActivity {
 
         PreviewView cameraPreviewView = findViewById(R.id.cameraPreviewView);
         preview.setSurfaceProvider(cameraPreviewView.getSurfaceProvider());
-
-        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(1280, 720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+        imageAnalysis.setAnalyzer(executor, new TextAnalyzer());
+        Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis, preview);
     }
 
     public void goBack(View view) {
         this.finish();
     }
+    private class TextAnalyzer implements ImageAnalysis.Analyzer {
+
+        @Override
+        public void analyze(@NonNull ImageProxy imageProxy) {
+            try (@SuppressLint("UnsafeOptInUsageError") Image mediaImage = imageProxy.getImage()) {
+                if (mediaImage != null) {
+                    InputImage image =
+                            InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+                    recognizeText(image);
+
+                }
+            }
+        }
+
+        private void recognizeText(InputImage image) {
+            TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+            Task<Text> result =
+                    recognizer.process(image)
+                            .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                @Override
+                                public void onSuccess(Text visionText) {
+                                    processTextBlock(visionText);
+                                }
+                            })
+                            .addOnFailureListener(
+                                    new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            // Task failed with an exception
+                                            // ...
+                                        }
+                                    });
+        }
+
+        private void processTextBlock(Text visionText) {
+            String resultText = visionText.getText();
+
+            for (Text.TextBlock block : visionText.getTextBlocks()) {
+                String blockText = block.getText();
+                Point[] blockCornerPoints = block.getCornerPoints();
+                Rect blockFrame = block.getBoundingBox();
+                for (Text.Line line : block.getLines()) {
+                    String lineText = line.getText();
+                    Point[] lineCornerPoints = line.getCornerPoints();
+                    Rect lineFrame = line.getBoundingBox();
+                    for (Text.Element element : line.getElements()) {
+                        String elementText = element.getText();
+                        Point[] elementCornerPoints = element.getCornerPoints();
+                        Rect elementFrame = element.getBoundingBox();
+                    }
+                }
+            }
+        }
+    }
+
 }
